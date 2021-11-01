4e97e81c17eabe9911eac66a0b1d25bd2c7c3ded
Maciej Błędkowski <23295125+mbledkowski@users.noreply.github.com>
2021-11-02 00:20:05 +0100

    Corrected connect.ts

--- api/src/db/connect.ts
+++ api/src/db/connect.ts
@@ -1,26 +1,13 @@
-// import knex from 'knex'
-// const pg = knex({
-//   client: 'pg',
-//   connection: {
-//     host : '127.0.0.1',
-//     port : 2137,
-//     user : 'postgres', //ENV
-//     password : 'password', //ENV
-//     database : 'postgres' //ENV?
-//   }
-// });
-
-const knex = require('knex')(
-  {
-    client: 'pg',
-    connection: {
-      host : '127.0.0.1',
-      port : 2137,
-      user : 'postgres', //ENV
-      password : 'password', //ENV
-      database : 'postgres' //ENV?
-    }
+import knex from 'knex'
+const pg = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : 2137,
+    user : 'postgres', //ENV
+    password : 'password', //ENV
+    database : 'postgres' //ENV?
   }
-)
+});
 
-export default knex
+export default pg
