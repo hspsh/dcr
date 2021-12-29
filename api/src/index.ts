04b2dfb3bbb4545115497f311ac217a996dc27fc
Maciej Błędkowski <23295125+mbledkowski@users.noreply.github.com>
2021-12-29 13:40:30 +0100

    Added response headers in index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,11 +7,14 @@ const app = express()
 const port = 8090
 const host = "localhost"
 
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.json())
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*") // TODO: change to only allow certain host
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
 app.use('/', router)
 app.use('/bucket',express.static(path.join(__dirname, '../public/bucket')))
 
