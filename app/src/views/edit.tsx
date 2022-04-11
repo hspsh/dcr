d3efdb66226bc71bc9e71687bd9b403edcb10292
Maciej Błędkowski <23295125+mbledkowski@users.noreply.github.com>
2022-04-11 17:11:10 +0200

    Imported styled-components, styled components (in app)

--- app/src/views/edit.tsx
+++ app/src/views/edit.tsx
@@ -2,7 +2,17 @@ import Header from '../components/Header'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect, useMemo, useCallback } from 'react'
 import mainApi from '../services/api'
-import _, { DebouncedFunc } from 'lodash'
+import _ from 'lodash'
+import styled from 'styled-components'
+
+const StyledTextArea = styled('textarea')`
+  width: calc(100% - 1rem);
+  height: calc(100% - 3rem);
+  > svg {
+    width: 100%;
+    height: 100%;
+  }
+`
 
 const Edit = () => {
   const { id } = useParams()
@@ -37,7 +47,7 @@ const Edit = () => {
   return (
     <>
       <Header id={id || ''} />
-      <textarea value={code} onChange={(e) => handleInput(e.currentTarget.value)} />
+      <StyledTextArea value={code} onChange={(e) => handleInput(e.currentTarget.value)} />
     </>
   )
 }
--- app/src/views/prev.tsx
+++ app/src/views/prev.tsx
@@ -1,11 +1,38 @@
 import Header from '../components/Header'
 import {useParams} from 'react-router-dom'
+import parser from 'html-react-parser'
+import mainApi from '../services/api'
+import { useEffect, useCallback, useMemo, useState } from 'react'
+import _ from 'lodash'
+import styled from 'styled-components'
+
+const Image = styled.div`
+  width: 100%;
+  height: calc(100% - 3rem);
+  > svg {
+    width: 100%;
+    height: 100%;
+  }
+`
 
 const Prev = () => {
-  const {id} = useParams()
+  const { id } = useParams()
+  const api = useMemo(() => new mainApi(id), [id])
+  const [img, setImg] = useState('')
+
+  const fetchDebounce = useCallback(
+    _.debounce(async () => {
+      setImg(await api.fetchImg().then(entry => entry.content))
+      fetchDebounce()
+    }, 3200), [])
+
+  useEffect(() => {
+    fetchDebounce()
+  }, [fetchDebounce])
   return (
     <>
       <Header id={id || ''}/>
+      <Image>{parser(img)}</Image>
     </>
   )
 }
